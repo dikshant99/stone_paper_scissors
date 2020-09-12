bfa4b5a9c6c627afa6d3ab3589a20c8988970e12
dikshant99 <42288957+dikshant99@users.noreply.github.com>
2020-09-13 03:14:41 +0530

    Stone-Paper-Scissors Game
    Game that entertain us between our work

---
+++ Stone_Paper_Game.py
@@ -0,0 +1,100 @@
+# Snake water gun
+
+import random
+
+lst = ['s', 'p', 'sc']
+
+chance = 5
+no_of_chance = 0
+computer_point = 0
+human_point = 0
+
+print(" \t \t \t \t Stone,Paper,Scissors Game\n \n")
+print("s for stone \np for paper \nsc for scissors \n")
+
+# making the game in while
+while no_of_chance < chance:
+    _input = input('Stone,Paper,Scissors:')
+    _random = random.choice(lst)
+
+    if _input == _random:
+        print("Tie Both 0 point to each \n ")
+
+    # if user enter s
+    elif _input == "s" and _random == "p":
+        computer_point = computer_point + 1
+        print(f"your guess {_input} and computer guess is {_random} \n")
+        print("computer wins 1 point \n")
+        print(f"computer_point is {computer_point} and your point is {human_point} \n ")
+
+    elif _input == "s" and _random == "sc":
+        human_point = human_point + 1
+        print(f"your guess {_input} and computer guess is {_random} \n")
+        print("Human wins 1 point \n")
+        print(f"computer_point is {computer_point} and your point is {human_point} \n")
+
+    # if user enter p
+    elif _input == "p" and _random == "sc":
+        computer_point = computer_point + 1
+        print(f"your guess {_input} and computer guess is {_random} \n")
+        print("computer wins 1 point \n")
+        print(f"computer_point is {computer_point} and your point is {human_point} \n ")
+
+    elif _input == "p" and _random == "s":
+        human_point = human_point + 1
+        print(f"your guess {_input} and computer guess is {_random} \n")
+        print("Human wins 1 point \n")
+        print(f"computer_point is {computer_point} and your point is {human_point} \n")
+
+    # if user enter sc
+
+    elif _input == "sc" and _random == "p":
+        human_point = human_point + 1
+        print(f"your guess {_input} and computer guess is {_random} \n")
+        print("Human wins 1 point \n")
+        print(f"computer_point is {computer_point} and your point is {human_point} \n")
+
+
+    elif _input == "sc" and _random == "s":
+        computer_point = computer_point + 1
+        print(f"your guess {_input} and computer guess is {_random} \n")
+        print("computer wins 1 point \n")
+        print(f"computer_point is {computer_point} and your point is {human_point} \n ")
+
+    else:
+        print("you have input wrong \n")
+
+    no_of_chance = no_of_chance + 1
+    print(f"{chance - no_of_chance} is left out of {chance} \n")
+
+print("Game over")
+
+if computer_point == human_point:
+    print("Tie")
+
+elif computer_point > human_point:
+    print("Computer wins and you loose")
+
+else:
+    print("you win and computer loose")
+
+print(f"your point is {human_point} and computer point is {computer_point}")
+
+#
+# Stone Paper Scissor Game in Python
+"""
+RULE-1 
+stone vs stone-> TIE WINS
+stone vs paper -> PAPER WINS
+stone vs scissors -> STONE WINS
+RULE-2
+paper vs paper -> TIE WINS
+paper vs stone -> PAPER WINS
+paper vs scissors -> SCISSORS WINS
+RULE-3
+scissors vs scissors -> TIE WINS
+scissors vs stone -> STONE WINS
+scissors vs paper -> SCISSORS WINS
+"""
+
+
